27bac1da15a2e7b2bfe9444f129ec3f503d0ed20
Rob <betoarpi@gmail.com>
2020-01-02 07:49:32 -0600

    Ejercicio reversestring: Solución #2

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,10 +7,24 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
+  //Create an empty string called "reversed"
+  let reversed = ''
+  //for each character in the provided string
+  //take a character and add it to the start of "reversed"
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+  //return the variable reversed
+  return reversed
+}
+
+module.exports = reverse;
+
+/* Solution #1
+  function reverse(str) {
   //convert the str into an array
   //reverse the array
   //join and return the reversed string
   return str.split('').reverse().join('')
-}
+} */
 
-module.exports = reverse;
