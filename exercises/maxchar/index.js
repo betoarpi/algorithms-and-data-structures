d23240a18e593e816eee6bb5863dd60f6a191125
Rob <betoarpi@gmail.com>
2020-01-07 09:11:03 -0600

    maxchar: solución recomendada

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {}
+  let max = 0
+  let maxChar = ''
+
+  // in the for loop, the word "of" is used for arrays and strings but not for objects
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++
+    } else {
+      charMap[char] = 1
+    }
+  }
+
+  // in the for loop, the word "in" is used for objects
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char]
+      maxChar = char
+    }
+  }
+
+  return maxChar
+}
 
 module.exports = maxChar;
